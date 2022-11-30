c17dc78b9555dbc140776b4b675ba9a3e6c8f45a
nikita-kotov <102904722+nikita-kotov@users.noreply.github.com>
2022-11-30 10:55:39 +0300

    Update Sync_mail_contacts.ps1
    Add filtering by existing mail and updating for groups

--- Sync_mail_contacts.ps1
+++ Sync_mail_contacts.ps1
@@ -8,14 +8,17 @@ Import-PSSession $session
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 Import-Module activedirectory
 Get-MailContact -OrganizationalUnit $TargetOU|Set-MailContact -CustomAttribute15 "checking"
-$Users = Get-ADUser -Server $DC -filter * -SearchBase $SourceOUUsers -SearchScope Subtree -Properties mail,company,department,l,mobile,physicalDeliveryOfficeName,streetAddress,telephoneNumber,title
-$groups = Get-ADGroup -Server $DC -Filter * -SearchBase $SourceOUGroups -SearchScope Subtree -Properties mail,mailNickname
+$Users = Get-ADUser -Server $DC -filter * -SearchBase $SourceOUUsers -SearchScope Subtree -Properties mail,company,department,l,mobile,physicalDeliveryOfficeName,streetAddress,telephoneNumber,title|
+    Where-Object -Property mail -NE $null
+$groups = Get-ADGroup -Server $DC -Filter * -SearchBase $SourceOUGroups -SearchScope Subtree -Properties mail,mailNickname|
+    Where-Object -Property mail -NE $null
 ForEach ($user in $users) {
     New-MailContact -name $user.name -Alias $user.SamAccountName -ExternalEmailAddress $user.mail -OrganizationalUnit $TargetOU
     Set-Contact -Identity $user.name -LastName $user.surname -FirstName $user.givenname -Phone $user.telephoneNumber -MobilePhone $User.mobile -Office $user.physicalDeliveryOfficeName -City $user.l -Title $user.title -Department $user.department -Company $user.company -StreetAddress $user.streetaddress
     Set-MailContact -Identity $user.name -CustomAttribute15 "updated"}
 ForEach ($group in $groups) {
     New-MailContact -name $group.name -Alias $group.mailNickname -ExternalEmailAddress $group.mail -OrganizationalUnit $TargetOU
+    Set-Contact -Identity $group.name -DisplayName $group.name
     Set-MailContact -Identity $group.name -CustomAttribute15 "updated"}
 Get-MailContact -OrganizationalUnit $TargetOU|
     Where-Object -Property CustomAttribute15 -EQ "checking"|
